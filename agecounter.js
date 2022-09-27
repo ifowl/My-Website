3bcbe83af287ba96b63856188bbbd77cb1f48369
Isaac Fowler <Isaac.scott.fowler@gmail.com>
2022-09-26 23:25:48 -0400

    Delete agecounter.js

--- agecounter.js
+++
@@ -1,6 +0,0 @@
-let ageEl = document.getElementById("age");
-
-setInterval(() => {
-	let time = dayjs().diff(dayjs("2001-03-08", "MM-DD-YYYY"), 'year', true);
-	ageEl.innerText = time.toString().substring(0, 12);
-}, 50);
